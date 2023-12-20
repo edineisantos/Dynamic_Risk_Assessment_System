6af06418eb8f3e56442984721a50fcc92730854f
Edinei Santos <7681342+edineisantos@users.noreply.github.com>
2023-12-20 17:13:51 -0300

    Completed Model Deployment Step

--- deployment.py
+++ deployment.py
@@ -1,27 +1,33 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import shutil
+
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl')
+score_path = os.path.join(config['output_model_path'], 'latestscore.txt')
+ingested_files_path = os.path.join(config['output_folder_path'],
+                                   'ingestedfiles.txt')
+
+# Function for model deployment
 
 
+def deploy_model():
+    # Copy trained model to the production deployment directory
+    shutil.copy(model_path, os.path.join(prod_deployment_path,
+                                         'trainedmodel.pkl'))
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
+    # Copy model score to the production deployment directory
+    shutil.copy(score_path, os.path.join(prod_deployment_path,
+                                         'latestscore.txt'))
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+    # Copy ingested files record to the production deployment directory
+    shutil.copy(ingested_files_path, os.path.join(prod_deployment_path,
+                                                  'ingestedfiles.txt'))
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == '__main__':
+    deploy_model()
