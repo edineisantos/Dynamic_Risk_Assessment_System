fa2163351317ba49ef07a878a973bc218de725a5
Edinei Santos <7681342+edineisantos@users.noreply.github.com>
2023-12-20 19:23:53 -0300

    Add parameters for model and test data to the score_model function

--- reporting.py
+++ reporting.py
@@ -10,18 +10,24 @@ from diagnostics import model_predictions
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-test_data_path = os.path.join(config['test_data_path'], 'testdata.csv')
-output_model_path = config['output_model_path']
+# Default test data and output model paths
+default_test_data_path = os.path.join(config['test_data_path'], 'testdata.csv')
+default_output_model_path = config['output_model_path']
 
 # Function for reporting
 
 
-def score_model():
+def score_model(test_data_file=default_test_data_path,
+                model_predictions_file=None):
     # Use the model_predictions function to get predictions
-    predictions = model_predictions()
+    if model_predictions_file is None:
+        predictions = model_predictions()
+    else:
+        predictions = model_predictions(model_path=model_predictions_file,
+                                        test_data_path=test_data_file)
 
     # Load the test data
-    test_data = pd.read_csv(test_data_path)
+    test_data = pd.read_csv(test_data_file)
 
     # Separate the y_test data (exited) from the test_data
     y_test = test_data['exited']
@@ -35,7 +41,14 @@ def score_model():
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
     plt.title('Confusion Matrix')
-    plt.savefig(os.path.join(output_model_path, 'confusionmatrix.png'))
+
+    # Save the confusion matrix plot to the output model path
+    if model_predictions_file is None:
+        plt.savefig(os.path.join(default_output_model_path,
+                                 'confusionmatrix.png'))
+    else:
+        output_dir = os.path.dirname(model_predictions_file)
+        plt.savefig(os.path.join(output_dir, 'confusionmatrix.png'))
 
 
 if __name__ == '__main__':
