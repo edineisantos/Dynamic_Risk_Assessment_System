e1af91e36dfe1301f98048366c26e09bbfd4e2b3
Edinei Santos <7681342+edineisantos@users.noreply.github.com>
2023-12-22 09:48:58 -0300

    Implement the 'apicalls' function to proceed with tests

--- apicalls.py
+++ apicalls.py
@@ -21,24 +21,28 @@ test_data_path = os.path.join(config['test_data_path'], 'testdata.csv')
 # Output directory path
 output_model_path = config['output_model_path']
 
-# Call each API endpoint and store the responses
-response1 = requests.post(PREDICTION_ENDPOINT,
-                          json={"test_data_path": test_data_path})
-response2 = requests.get(SCORING_ENDPOINT)
-response3 = requests.get(SUMMARY_STATS_ENDPOINT)
-response4 = requests.get(DIAGNOSTICS_ENDPOINT)
+def apicalls():
+    # Call each API endpoint and store the responses
+    response1 = requests.post(PREDICTION_ENDPOINT,
+                            json={"test_data_path": test_data_path})
+    response2 = requests.get(SCORING_ENDPOINT)
+    response3 = requests.get(SUMMARY_STATS_ENDPOINT)
+    response4 = requests.get(DIAGNOSTICS_ENDPOINT)
 
-# Combine all API responses
-responses = {
-    "prediction": response1.json(),
-    "scoring": response2.json(),
-    "summary_stats": response3.json(),
-    "diagnostics": response4.json()
-}
+    # Combine all API responses
+    responses = {
+        "prediction": response1.json(),
+        "scoring": response2.json(),
+        "summary_stats": response3.json(),
+        "diagnostics": response4.json()
+    }
 
-# Define the path for apireturns.txt in the output_model_path directory
-apireturns_path = os.path.join(output_model_path, "apireturns.txt")
+    # Define the path for apireturns.txt in the output_model_path directory
+    apireturns_path = os.path.join(output_model_path, "apireturns.txt")
 
-# Write the responses to the apireturns.txt file
-with open(apireturns_path, "w") as outfile:
-    json.dump(responses, outfile, indent=4)
+    # Write the responses to the apireturns.txt file
+    with open(apireturns_path, "w") as outfile:
+        json.dump(responses, outfile, indent=4)
+
+if __name__ == '__main__':
+    apicalls()
